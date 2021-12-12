0b38fab4eb170806b15b7702638847c91c48cc34
katelynjohnnson <89552485+katelynjohnnson@users.noreply.github.com>
2021-12-11 19:28:59 -0600

    django v

--- pizzeria/urls.py
+++ pizzeria/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pizzas', views.pizzas, name='pizzas'),
-    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
+    path('',include('pizzas.urls')),
+    path('users/',include('users.urls')),
 ]
